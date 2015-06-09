a97b6382a496eb757f4944405313928699c6ce85
Jason L Perry <jasper@ambethia.com>
2015-06-09 12:33:50 -0400

    Memoize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # TODO: Memoize
+  # Memoization
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   helper_method :current_user
 
