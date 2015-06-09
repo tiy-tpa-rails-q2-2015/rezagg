4f4df170a44681c1ec3c56e61722c0a5977b3c86
Jason L Perry <jasper@ambethia.com>
2015-06-09 10:18:37 -0400

    Bookmark scaffold and Bootstrap

---
+++ db/migrate/20150609141234_create_bookmarks.rb
@@ -0,0 +1,12 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.references :user, index: true, foreign_key: true
+      t.string :url
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
